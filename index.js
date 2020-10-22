87e39cde8ba419f19d1f4799ceeca4c7bed668b4
JinitSan <jinitsanghvi@gmail.com>
2020-10-22 09:33:36 +0530

    2 Forms set up

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const app = express();
+const mysql = require("mysql")
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -10,6 +11,49 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
     res.send("<h1>College Registration System</h1>")
 })
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    console.log(req.body)
+    console.log(req.body['mis'])
+    console.log(req.body['password'])
+})
+
+app.get("/register",function(req,res){
+    res.render("details");
+})
+
+app.post("/register",function(req,res){
+    console.log(req.body);
+    res.redirect("/course_dept")
+})
+
+app.get("/course_dept",function(req,res){
+    res.render("course_dept");
+});
+
+app.post("/course_dept",function(req,res){
+    console.log(req.body);
+})
+
+// var con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database:"student_reg"
+//   });
+  
+// con.connect(function(err){
+//     if (err) throw err;
+//     console.log("Connected!");
+//     con.query("use student_reg", function (err, result) {
+//         if (err) throw err;
+//         console.log("Database used");
+//     });
+// });
 
 app.listen(3000,function(){
     console.log("Server started at 3000");
