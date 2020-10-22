18c365c5a1272ae067a0e382780be553918ddf5e
JinitSan <jinitsanghvi@gmail.com>
2020-10-22 14:21:37 +0530

    Added course form

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database:"student_reg"
+});
+
 app.get("/",function(req,res){
     res.send("<h1>College Registration System</h1>")
 })
@@ -38,22 +45,6 @@ app.get("/course_dept",function(req,res){
 app.post("/course_dept",function(req,res){
     console.log(req.body);
 })
-
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "password",
-//     database:"student_reg"
-//   });
-  
-// con.connect(function(err){
-//     if (err) throw err;
-//     console.log("Connected!");
-//     con.query("use student_reg", function (err, result) {
-//         if (err) throw err;
-//         console.log("Database used");
-//     });
-// });
 
 app.listen(3000,function(){
     console.log("Server started at 3000");
